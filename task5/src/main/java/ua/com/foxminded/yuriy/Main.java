1fd0e6c771ee3e5e6af407335fe6a0156845999b
Yuriy Honcharov <yuriyhoncharov@gmail.com>
2023-04-18 18:50:15 +0200

    Initial Commit Task 5

---
+++ task5/src/main/java/ua/com/foxminded/yuriy/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package ua.com.foxminded.yuriy;
+
+/**
+ * @author Yura
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
