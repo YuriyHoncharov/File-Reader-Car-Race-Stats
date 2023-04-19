59a752e554b9db5a0936fff6fd9e11b2d6572698
Yuriy Honcharov <yuriyhoncharov@gmail.com>
2023-04-19 22:57:36 +0200

    i'm shocked, but i done it :3

--- task5/src/main/java/ua/com/foxminded/yuriy/DataRead.java
+++ task5/src/main/java/ua/com/foxminded/yuriy/DataRead.java
@@ -3,21 +3,24 @@ package ua.com.foxminded.yuriy;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DataRead {
 
-	public List<String> getArrayList(File file) {
+	private List<String> getArrayList(File file) {
 		List<String> result = null;
 		try (Stream<String> stream = Files.lines(file.toPath())) {
-//			result = stream.collect(Collectors.toList());
-			result = stream.collect(Collectors.toList());
+			result = stream.sorted().collect(Collectors.toList());
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -25,20 +28,78 @@ public class DataRead {
 		return result;
 	}
 
-	public Map<String, LocalDateTime> getTimeResult(List<String> input) {
+	public LinkedHashMap<String, LinkedHashMap<String, String>> getAbbreviation(File file) {
+		List<String> tempList = getArrayList(file);
+		return tempList.stream().sorted(Comparator.comparing(str -> str.split("_")[0])).collect(LinkedHashMap::new,
+				(map, str) -> {
+					String[] data = str.split("_");
+					String abbreviationRacer = data[0];
+					String abbreviationRacerName = data[1];
+					String abbreviationRacerTeam = data[2];
+					map.computeIfAbsent(abbreviationRacer, k -> new LinkedHashMap<>()).put(abbreviationRacerName,
+							abbreviationRacerTeam);
+				}, LinkedHashMap::putAll);
+	}
+
+	public LinkedHashMap<String, LocalDateTime> getTimeResult(File file) {
+		List<String> input = getArrayList(file);
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH:mm:ss.SSS");
-		return input.stream().collect(HashMap::new,
+		return input.stream().sorted().collect(LinkedHashMap::new,
 				(map, str) -> map.put(str.substring(0, 3), LocalDateTime.parse(str.substring(3), formatter)),
-				HashMap::putAll);
+				LinkedHashMap::putAll);
 	}
 
-	public Map<String, LocalDateTime> calcolateResult(Map<String, LocalDateTime> endTime,
-			Map<String, LocalDateTime> startTime) {
-		
-
-		
-
-		return startTime;
+	public LinkedHashMap<String, String> calcolateResult(LinkedHashMap<String, LocalDateTime> endTime,
+			LinkedHashMap<String, LocalDateTime> startTime) {
+		return endTime.entrySet().stream().filter(entry -> startTime.containsKey(entry.getKey()))
+				.sorted(Map.Entry.comparingByKey()).collect(Collectors.toMap(Map.Entry::getKey, entry -> {
+					Duration duration = Duration.between(startTime.get(entry.getKey()), endTime.get(entry.getKey()));
+					long minutes = duration.toMinutes();
+					long seconds = duration.getSeconds() % 60;
+					long millis = duration.toMillis() % 1000;
+					return String.format("%d:%02d.%03d", minutes, seconds, millis);
+				}, (v1, v2) -> v1, LinkedHashMap::new));
 	}
-
+	
+	public String printResult (LinkedHashMap<String, String> results, LinkedHashMap<String, LinkedHashMap<String, String>> driverData, int topResultsNumber ) {
+		StringBuilder resultPrint = new StringBuilder();
+		AtomicInteger count = new AtomicInteger(1);
+		results.entrySet().stream()
+		.sorted(Map.Entry.comparingByValue())
+		.limit(topResultsNumber)
+		.forEachOrdered(entry -> {
+			String driverAbbviation = entry.getKey();
+			String time = entry.getValue();
+			LinkedHashMap<String, String> driverNameAndTeam = driverData.get(driverAbbviation);
+			String driverName = driverNameAndTeam.keySet().iterator().next();
+			String driverTeam = driverNameAndTeam.values().iterator().next();
+			String outTopResults = String.format("%-2d. %-20s | %-30s | %s\n",
+					count.getAndIncrement(),
+					driverName,
+					driverTeam,
+					time);
+			resultPrint.append(outTopResults);
+	}
+		);
+	resultPrint.append("---------------------------------\n");
+				
+		results.entrySet().stream()
+		.sorted(Map.Entry.comparingByValue())
+		.skip(topResultsNumber)
+		.forEachOrdered(entry -> {
+			String driverAbbviation = entry.getKey();
+			String time = entry.getValue();
+			LinkedHashMap<String, String> driverNameAndTeam = driverData.get(driverAbbviation);
+			String driverName = driverNameAndTeam.keySet().iterator().next();
+			String driverTeam = driverNameAndTeam.values().iterator().next();
+			String outTopResults = String.format("%-2d. %-20s | %-30s | %s\n",
+					count.getAndIncrement(),
+					driverName,
+					driverTeam,
+					time);
+			resultPrint.append(outTopResults);
+		});
+		return resultPrint.toString();
+	}
 }
+
--- task5/src/main/java/ua/com/foxminded/yuriy/Main.java
+++ task5/src/main/java/ua/com/foxminded/yuriy/Main.java
@@ -8,8 +8,10 @@ import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -25,23 +27,18 @@ public class Main {
 
 		File fileStart = new File(path + startLogPath);
 		File fileEnd = new File(path + endLogPath);
-		File fileAbbreviation = new File(path + abbreviationLogPath);
-		
-		
-		List<String> startList = new ArrayList<String>();
-		List<String> endList = new ArrayList<String>();
-		List<String> abbList = new ArrayList();
-		
+		File fileAbbreviation = new File(path + abbreviationLogPath);		
+			
 		DataRead formulaUno = new DataRead();
-		startList = formulaUno.getArrayList(fileStart);
-		endList = formulaUno.getArrayList(fileEnd);
+				
+		LinkedHashMap<String, LocalDateTime> startTime = formulaUno.getTimeResult(fileStart);
+		LinkedHashMap<String, LocalDateTime> endTime = formulaUno.getTimeResult(fileEnd);
+		LinkedHashMap<String, LinkedHashMap<String, String>> abbreviation = formulaUno.getAbbreviation(fileAbbreviation);
+		LinkedHashMap<String, String> resultRace = formulaUno.calcolateResult(endTime, startTime);
 		
-		Map<String, LocalDateTime> startTime = formulaUno.getTimeResult(startList);
-		Map<String, LocalDateTime> endTime = formulaUno.getTimeResult(endList);
+		String result = formulaUno.printResult(resultRace, abbreviation, 15);
+		System.out.println(result);
 		
-		for (String s : startList) {
-			System.out.println(s);
-		}
 		
 		
 				
